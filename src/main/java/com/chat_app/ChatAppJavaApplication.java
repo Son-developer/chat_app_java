7ac0ce7215dc6349a2c1fe4d70ccf05a0ae7992c
vanthaison <vanthaison2008.tp@gmail.com>
2026-07-03 12:11:03 +0700

    Fix syntax error in application.properties

--- src/main/java/com/chat_app/ChatAppJavaApplication.java
+++ src/main/java/com/chat_app/ChatAppJavaApplication.java
@@ -1,11 +1,14 @@
 package com.chat_app;
 
+// Nhà máy điều phối mọi thứ
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// khai báo lớp ChatAppJavaApplication để chạy ứng dụng Spring Boot
 @SpringBootApplication
 public class ChatAppJavaApplication {
 
+	// phương thức main để chạy ứng dụng Spring Boot
 	public static void main(String[] args) {
 		SpringApplication.run(ChatAppJavaApplication.class, args);
 	}
